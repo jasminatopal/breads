de6a55750f6a354c02d6f4bb43953f88fb492941
unknown <jasmina_t05@outlook.com>
2023-03-14 21:22:56 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
 
 
 // ROUTES
@@ -18,14 +21,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
 
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
 
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page - always make this last
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {

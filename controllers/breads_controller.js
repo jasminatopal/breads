1abf21b1009bf49f04c2d7d72dc67483440dc0b8
unknown <jasmina_t05@outlook.com>
2023-03-14 19:03:55 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,16 @@ const Bread = require('../models/bread.js')
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
 
 // INDEX
 breads.get('/', (req, res) => {

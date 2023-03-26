55790232069b88c650a06593bce032f27c445521
unknown <jasmina_t05@outlook.com>
2023-03-25 17:02:13 -0700

    added mongoose

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// DEPENDENCIES
-const express = require('express')
-
-// CONFIGURATION
+// CONFIGURATION//Dependencies
 require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-
-// DEPENDENCIES
+const express = require('express')
 const methodOverride = require('method-override')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}) .then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
+
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
@@ -17,7 +17,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 
 // ROUTES
@@ -25,10 +27,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
 
-
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
 
 // 404 Page - always make this last
 app.get('*', (req, res) => {
@@ -39,3 +37,5 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
+

517f6dd8149551c88f60a352b751b8fccaa3de54
unknown <jasmina_t05@outlook.com>
2023-03-25 18:32:44 -0700

    added helper-methods-with-options

--- views/show.jsx
+++ views/show.jsx
@@ -18,12 +18,11 @@ function Show ({bread, index}) {
   </p>
   <img src={bread.image} alt={bread.name} />
 
-  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+  <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
-
-  <form action={`/breads/${index}?_method=DELETE`} method="POST">
+  <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
-  </form>
+</form>
 
   <form action={`/breads/${index}?_method=PUT`} method="POST"></form>
 
